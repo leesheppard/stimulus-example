2e731e998e6960c6932550e9c908a708a5c7e334
Lee Sheppard <lee@leesheppard.com>
2022-11-14 11:11:04 +1100

    Create toggle controller

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,6 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  connect() {
-  }
+    toggle(e) {
+        e.preventDefault();
+        this.element.classList.toggle(this.cssClass);
+    }
+
+    get cssClass() {
+        return this.data.get("cssClass");
+    }
 }
